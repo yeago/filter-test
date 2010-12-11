e7c1943c3c4570786ca102dba88d5adf4dc4e1eb
Stephen <subsume@gmail.com>
2010-12-11 01:38:59 +0000

    Added a test illustrating the problem with chained filtering

--- filtertest/tests.py
+++ filtertest/tests.py
@@ -1,23 +1,15 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+from filtertest import models as fm
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+class FilterTest(TestCase):
+    def test_basic_assumption(self):
+		company = fm.Company.objects.create(name="ACME")
 
->>> 1 + 1 == 2
-True
-"""}
+		fm.Location.objects.create(company=company,name="West Location",zip_code="90210",open_days="WEEK")
+		fm.Location.objects.create(company=company,name="East Location",zip_code="32801",open_days="WEEKEND")
 
+		"""
+		Fail if our filtering for a WEEKEND company in 90210 finds our company above
+		"""
+		self.failUnlessEqual(fm.Company.objects.filter(locations__zip_code="90210").filter(locations__open_days="WEEKEND").count(),0)
